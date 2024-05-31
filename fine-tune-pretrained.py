1d7cbc1188843214d74ac64c5829f8b4aefe82b9
Jotham Teshome <jothamteshome@gmail.com>
2024-05-31 01:28:41 -0400

    Moved dataset loading to separate file and modified steps to tune models

--- fine-tune-pretrained.py
+++ fine-tune-pretrained.py
@@ -1,63 +1,24 @@
-import math
-import torch
-
-from trl import SFTTrainer, setup_chat_format
+from load_datasets import load_split_dataset
+from torch import bfloat16
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, TrainingArguments   
-from datasets import load_dataset, Dataset
+from trl import SFTTrainer, setup_chat_format
 
 
-# Load the General-Knowledge dataset in the correct format
-def load_general_knowledge_dataset():
-    # Load the training split of the General-Knowledge dataset from HuggingFace datasets
-    dataset = load_dataset("MuskumPillerum/General-Knowledge", split='train').to_pandas()
-
-    # Drop all rows with None values in dataset
-    dataset.dropna(inplace=True)
-
-    # Format data into typical format used by trainer
-    data = []
-
-    # Add questions as user messages and answers as assistant messages
-    for row in dataset.itertuples():
-        row_data = {"messages": [], "prompt_id": str(row[0])}
-        row_data["messages"].append({"role": "user", "content": row[2]})
-        row_data["messages"].append({"role": "assistant", "content": row[1]})
-        data.append(row_data)
-
-    # Convert from dictionary back to Huggingface Dataset
-    dataset = Dataset.from_list(data)
-
-    # Split the dataset into a training and validation set
-    dataset = dataset.train_test_split(test_size=0.1, seed=42)
-
-    return dataset
-
-
-# Load the Ultrachat 200k dataset in the correct format
-def load_ultrachat_dataset():
-    # Load the training split of the Ultrachat 200k dataset from HuggingFace datasets
-    dataset = load_dataset("HuggingFaceH4/ultrachat_200k", split="train_sft")
-
-    # Split the dataset into a training and validation set
-    dataset = dataset.train_test_split(test_size=0.1, seed=42)
-
-    return dataset
-
-
-# Returnds the directory and dataset corresponding to the dataset name passed in
-def load_split_dataset(dataset_name, model_name):
-    model_name = model_name.split("/")[-1]
-
-    datasets = {'general_knowledge': load_general_knowledge_dataset, 'ultrachat_200k': load_ultrachat_dataset}
-    
-    return f"{dataset_name}-{model_name}-model", datasets[dataset_name]()
-
-
-# Function to handle the fine-tuning of the DistilGPT2 model using a given dataset
+# Function to handle the fine-tuning of the model using a given dataset
 def run_training(dataset_name, model_name):
-    # Load pretrained model and tokenizer for DistilGPT2
-    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+    # Load pretrained model and tokenizer
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=bfloat16)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Add new tokens when running on bitext_customer support dataset
+    if dataset_name == "bitext_customer_support":
+        with open("bitext_customer_support_tokens.txt", "r") as f:
+            new_tokens = "".join(f.readlines()).strip().split("\n")
+
+        new_tokens = set(new_tokens) - set(tokenizer.vocab.keys())
+        tokenizer.add_tokens(list(new_tokens))
+        model.resize_token_embeddings(len(tokenizer))
+
 
     # Set up the chat format for the model
     model, tokenizer = setup_chat_format(model, tokenizer)
@@ -67,27 +28,27 @@ def run_training(dataset_name, model_name):
 
     # Set training arguments for the trainer
     training_args = TrainingArguments(directory,
-                                        eval_strategy='epoch',
-                                        learning_rate=5e-4,
+                                        eval_strategy='no',
+                                        num_train_epochs=7,
+                                        learning_rate=5e-5,
                                         weight_decay=1e-5,
-                                        logging_strategy="no",
-                                        save_steps=5000)
+                                        per_device_train_batch_size=1,
+                                        gradient_accumulation_steps=4,
+                                        bf16=True,
+                                        logging_strategy="epoch",
+                                        save_total_limit=1,
+                                        )
 
     # Set up the supervised fine-tuning trainer from TRL
     trainer = SFTTrainer(model=model,
                         tokenizer=tokenizer,
                         args=training_args,
-                        train_dataset=dataset['train'],
-                        eval_dataset=dataset['test'],
-                        max_seq_length=512)
+                        train_dataset=dataset,
+                        max_seq_length=1024)
 
 
     # Train the model
     trainer.train()
-
-    # Evaluate performance using perplexity
-    evaluation_results = trainer.evaluate()
-    print(f"Perplexity: {math.exp(evaluation_results['eval_loss']):.2f}")
 
     # Save the final fine-tuned model
     trainer.save_model(f"{directory}/tuned_model")
@@ -102,7 +63,7 @@ def generate_response(model, tokenizer, chat):
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
     # Generate a response using the chat history
-    response = pipe(chat, max_new_tokens=128, truncation=True)
+    response = pipe(chat, max_new_tokens=32, truncation=True)
 
     # Print the most recent response
     print(response[0]['generated_text'][-1]['content'])
@@ -116,16 +77,19 @@ def run_inference(dataset_name, model_name):
     model_name = model_name.split("/")[-1]
     
     # Load in the fine-tuned model and tokenizer
-    model = AutoModelForCausalLM.from_pretrained(f"{dataset_name}-{model_name}-model/tuned_model", torch_dtype=torch.bfloat16)
+    model = AutoModelForCausalLM.from_pretrained(f"{dataset_name}-{model_name}-model/tuned_model", torch_dtype=bfloat16)
     tokenizer = AutoTokenizer.from_pretrained(f"{dataset_name}-{model_name}-model/tuned_model")
 
+    # Make sure that model token embeddings contain any new tokens
+    model.resize_token_embeddings(len(tokenizer))
+
     # Initialize the chat with a generic system message
-    chat = [{"role":"system", "content": "You are very helpful chatbot who responds one answer at a time."}]
+    chat = []
 
     # Loop forever
     while True:
         # Add the current user input message to the chat history
-        chat.append({'role': "user", "content": input(">> ")})
+        chat.append({'role': "user", "content": input(">> ") + tokenizer.eos_token})
 
         # Run inference on the current chat history and update the chat history
         # for future inference
@@ -133,7 +97,7 @@ def run_inference(dataset_name, model_name):
 
 
 if __name__ == "__main__":
-    dataset_name = "ultrachat_200k"
-    model_name = "distilbert/distilgpt2"
+    dataset_name = "bitext_customer_support"
+    model_name = "microsoft/DialoGPT-medium"
     run_training(dataset_name, model_name)
-    # run_inference(dataset_name)
+    # run_inference(dataset_name, model_name)
---
+++ load_datasets.py
@@ -0,0 +1,125 @@
+from convokit import Corpus, download
+from datasets import load_dataset, Dataset
+from natsort import index_natsorted
+
+# Search through and format single conversation between two characters
+def search_conversation(utterances, conv_id):
+    # Initialize conversation data object
+    conv_data = {"messages": [], "prompt_id": str(conv_id)}
+
+    # Select utterances with the current conversation id
+    conversation = utterances[utterances['conversation_id'] == conv_id]
+
+    # Use first speaker as "user" and second speaker as "assistant" 
+    user_message = True
+
+    # Loop through utterances and add to conversation data object
+    for utterance in conversation.itertuples():
+        # Append conversation data
+        conv_data['messages'].append({"role": "user" if user_message else "assistant", "content": utterance[2]})
+
+        # Flip user message state
+        user_message = not user_message
+
+    return conv_data
+
+
+# Load the Movie-Dialogs Corpus in the correct format
+def load_movie_dialog_dataset():
+    # Load corpus using convokit
+    corpus = Corpus(filename=download("movie-corpus"))
+
+    # Load utterance data as pandas dataframe
+    utterances = corpus.get_utterances_dataframe()
+
+    # Sort utterances by id
+    utterances = utterances.iloc[index_natsorted(utterances.index)]
+
+    # Find all unique conversation ids
+    conv_ids = utterances.conversation_id.unique()
+
+    # Store conversation data in list
+    data = []
+
+    # Loop through conversation ids and build dataset
+    for conv_id in conv_ids:
+        data.append(search_conversation(utterances, conv_id))
+
+    # Convert from list of dictionaries back to Huggingface Dataset
+    dataset = Dataset.from_list(data)
+
+    return dataset
+
+
+# Load the Bitext Customer Support dataset in the correct format
+def load_bitext_customer_support_dataset():
+    # Load the training split of the Bitext Customer Support dataset from HuggingFace datasets
+    dataset = load_dataset("bitext/Bitext-customer-support-llm-chatbot-training-dataset", split='train').to_pandas()
+
+    # Format data into typical format used by trainer
+    data = []
+
+    # Add questions as user messages and answers as assistant messages
+    for row in dataset.itertuples():
+        row_data = {"prompt": row[2], "messages": [], "prompt_id": row[0]}
+        row_data["messages"].append({"role": "user", "content": row[2]})
+        row_data["messages"].append({"role": "assistant", "content": row[5]})
+        data.append(row_data)
+
+    # Convert from list of dictionaries back to Huggingface Dataset
+    dataset = Dataset.from_list(data)
+
+    return dataset
+
+
+# Load the Code-Feedback dataset in the correct format
+def load_code_feedback_dataset():
+    # Load the training split of the Code-Feedback dataset from HuggingFace datasets
+    dataset = load_dataset("m-a-p/Code-Feedback", split="train")
+
+    return dataset
+
+
+# Load the General-Knowledge dataset in the correct format
+def load_general_knowledge_dataset():
+    # Load the training split of the General-Knowledge dataset from HuggingFace datasets
+    dataset = load_dataset("MuskumPillerum/General-Knowledge", split='train').to_pandas()
+
+    # Drop all rows with None values in dataset
+    dataset.dropna(inplace=True)
+
+    # Format data into typical format used by trainer
+    data = []
+
+    # Add questions as user messages and answers as assistant messages
+    for row in dataset.itertuples():
+        row_data = {"messages": [], "prompt_id": str(row[0])}
+        row_data["messages"].append({"role": "user", "content": row[2]})
+        row_data["messages"].append({"role": "assistant", "content": row[1]})
+        data.append(row_data)
+
+    # Convert from list of dictionaries back to Huggingface Dataset
+    dataset = Dataset.from_list(data)
+
+    return dataset
+
+
+# Load the Ultrachat 200k dataset in the correct format
+def load_ultrachat_dataset():
+    # Load the training split of the Ultrachat 200k dataset from HuggingFace datasets
+    dataset = load_dataset("HuggingFaceH4/ultrachat_200k", split="train_sft")
+
+    return dataset
+
+
+# Returns the directory and dataset corresponding to the dataset name passed in
+def load_split_dataset(dataset_name, model_name):
+    model_name = model_name.split("/")[-1]
+
+    datasets = {'general_knowledge': load_general_knowledge_dataset, 
+                'ultrachat_200k': load_ultrachat_dataset,
+                'code_feedback': load_code_feedback_dataset,
+                'movie_dialog_corpus': load_movie_dialog_dataset,
+                'bitext_customer_support': load_bitext_customer_support_dataset}
+    
+    return f"{dataset_name}-{model_name}-model", datasets[dataset_name]()
